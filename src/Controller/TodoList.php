d1fa0af2528737ac97fe123ac5b7f0c8fddd206e
Oussama <Oussama@mail.com>
2020-07-07 18:34:22 +0100

    WIP

--- src/Controller/TodoList.php
+++ src/Controller/TodoList.php
@@ -4,18 +4,20 @@
 namespace App\Controller;
 
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class TodoList
 {
-    public function getTodoList() : Response
+    public function getTodoList(Request $request) : JsonResponse
     {
         $myTodoList = [
             $this->buildTodo(1, "some task", false),
             $this->buildTodo(2, "some other task", false),
             $this->buildTodo(3, "send valid todo list", true),
             array("id"=>4, "label"=>"new label", "checked"=>false)];
-        return new Response(json_encode($myTodoList));
+
+        return JsonResponse::create($myTodoList);
     }
 
     private function buildTodo($id, $label, $checked){
---
+++ src/Entity/Todo.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class Todo
+{
+    private $id;
+
+    private $label;
+
+    private $checked;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param mixed $label
+     */
+    public function setLabel($label): void
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChecked()
+    {
+        return $this->checked;
+    }
+
+    /**
+     * @param mixed $checked
+     */
+    public function setChecked($checked): void
+    {
+        $this->checked = $checked;
+    }
+
+}
